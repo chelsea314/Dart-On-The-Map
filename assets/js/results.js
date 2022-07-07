6393775a5b7ff10d712e6fc52cc96e7f61cdc4b7
unknown <jadentr.44@gmail.com>
2022-07-07 14:40:26 -0500

    added html dropdow, working with api

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,4 +1,9 @@
-var myHeaders = new Headers();
+$('#convert-Btn').on('click',convertCurrency)
+
+
+
+function convertCurrency(){
+  var myHeaders = new Headers();
 myHeaders.append("apikey", "UB6XaduROqPg9b6Y4lom3zo2FNX1CVL4");
 
 var requestOptions = {
@@ -6,14 +11,15 @@ var requestOptions = {
   redirect: 'follow',
   headers: myHeaders
 };
+  let convertingAmount = $("#amount").val()
+  let fromCurrency = $("#from").val()
+  let toCurrency =  $("#to").val()
 
-fetch("https://api.apilayer.com/exchangerates_data/convert?to=JPY&from=USD&amount=100", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
+  fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${toCurrency}&from=${fromCurrency}&amount=${convertingAmount}`, requestOptions)
+  .then(response => response.json())
+  .then(data => {
+     $('#converted').text(data.result)
+  })
   .catch(error => console.log('error', error));
 
-function convertCurrency(){
-  let convertingAmount = $("#amount").val()
-  let fromCurrency = "USD"
-  
 }
